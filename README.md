8a2f2c6c1e97d76ee5dc7c70f3b58dff23b436a1
Isaac <jordm1892@gmail.com>
2026-02-23 11:13:53 +0100

    Modification du timer et du README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Fonctions-Stage
-Contient toutes les fonctions faites
+# FONCTIONS-STAGE
+Il contient toutes les fonctions utilitaires sur les **tableaux** et les **chaines de caractères** faites en **Javascript** et leurs explications, ainsi que le **timer** réalisé, le style qui lui a été appliqué et comment les méthodes utilisées marchent.
+
+## I-Fonctions Javascript
--- timer.js
+++ timer.js
@@ -1,44 +1,16 @@
 
-const mins=document.getElementById("mins");
-const sec=document.getElementById("secs");
-const hrs=document.getElementById("hrs")
-let secondes=0;
-let minute=0;
-let heures=0;
+    function timeAct(){
+        const heureActuelle=new Date()
+        let secondes=heureActuelle.getSeconds();
+        let minutes=heureActuelle.getMinutes();
+        let heures=heureActuelle.getHours();
+        secondes=secondes<10 ? "0" + secondes:secondes
+        const heureFinale= heures + ":" + minutes + ":" + secondes 
+        const styleHeure=document.getElementById("heureActu")
+        styleHeure.textContent= heureFinale
 
-   
-  setInterval(() => {
-      secondes++   
-      sec.textContent=secondes;
-         if(secondes<10){
-            sec.textContent="0" + secondes;
-        }
-        if(secondes>59){
-            secondes=0;
-            sec.textContent="0" + secondes;
-        }
-    }, 1000);
-
-     setInterval(minutes, 60000);
-
-    function minutes(){
-        mins.textContent=minute + ":";
-        minute++;
-         if(minute<10){
-            mins.innerText="0" + minute + ":";
-        }
-        if(minute>60){
-            minute=0
-        }
-    };
-
-  setInterval(heures,360000);
-
-    function heure(){
-        hrs.textContent=heures
-        heures++
-        if(heures>23){
-            heures=0
-        }
     }
- 
+
+    setInterval(timeAct,1000)
+
+    
